8755959da1c1c731f804487824d55bfe8c00bbd6
Gormlaith Melisandre <gmelisan@student.21-school.ru>
2018-12-20 18:01:04 +0300

    upd

--- preparation.c
+++ preparation.c
@@ -6,7 +6,7 @@
 /*   By: gmelisan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 13:25:56 by gmelisan          #+#    #+#             */
-/*   Updated: 2018/12/20 17:06:32 by gmelisan         ###   ########.fr       */
+/*   Updated: 2018/12/20 18:00:46 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		ft_get_lines(int fd, char ***lines, int n)
 
 	if (n <= 0 || fd < 0 || !lines)
 		return (0);
-	*lines = ft_memalloc(sizeof(*lines) * (n + 1));
+	*lines = ft_memalloc(sizeof(**lines) * (n + 1));
 	i = 0;
 	while (i < n)
 	{
@@ -51,6 +51,35 @@ int		check_tet(char **lines)
 	return (1);
 }
 
+void	push(t_list *tetlist, char **lines)
+{
+	t_tetrimino	*tet;
+	t_coord		*coord;
+	int			i;
+	int			j;
+	int			k;
+
+	tet = ft_memalloc(sizeof(*tet));
+	i = 0;
+	k = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (lines[i][j] == '#')
+			{
+				coord = ft_memalloc(sizeof(*coord));
+				coord->x = j;
+				coord->y = i;
+				tet->coord[k++] = coord;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 t_list	*handle_file(char *path)
 {
 	int			fd;
@@ -67,9 +96,9 @@ t_list	*handle_file(char *path)
 		ft_assert(ret == 4); 
 		if (check_tet(lines))
 		{
-			ft_lstaddback(&tetlist, ft_lstnew(NULL, 0));
-			
+			push(tetlist, lines);
 		}
 	}
-	return (NULL);
+	return (tetlist);
 }
+
