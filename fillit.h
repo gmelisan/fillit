928368416af7651e1207d067ad001695c7a4db04
Vladimir <nefyodovve@gmail.com>
2018-12-21 21:36:02 +0000

    fillit.h upd

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gmelisan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 12:38:00 by gmelisan          #+#    #+#             */
-/*   Updated: 2018/12/20 18:00:41 by gmelisan         ###   ########.fr       */
+/*   Updated: 2018/12/21 18:57:38 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,20 @@ typedef struct	s_coord
 	int y;
 }				t_coord;
 
-typedef struct	s_tetrimino
+typedef struct		s_tet
 {
-	int			width;
-	int			height;
-	t_coord		coord[4];
-	t_coord		abs_coord;
-}				t_tetrimino;
+	int				width;
+	int				height;
+	t_coord			coord[4];
+	t_coord			abs_coord;
+	struct s_tet	*next;
+}					t_tet;
 
 void		ft_assert(int exp);
-t_list		*handle_file(char *path);
-int			fillit(t_list *tetlist);
+t_tet		*handle_file(char *path);
+int			fillit(t_tet *tet);
 void		display_usage(void);
-void		display_solution(t_list *tetlist, int n);
+void		display_solution(t_tet *tet, int n);
+void		delete_tets(t_tet **tet);
 
 #endif
